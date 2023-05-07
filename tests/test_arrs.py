798c6fed47458a71b04df52936ee06385f60f596
blau <serzh.plichko@mail.ru>
2023-05-07 15:42:33 +1100

    covered 100% codes by tests

--- tests/test_arrs.py
+++ tests/test_arrs.py
@@ -6,6 +6,7 @@ class TestArrs(unittest.TestCase):
 
     def test_get(self):
         self.assertEqual(arrs.get([1, 2, 3], 2, "test"), 3)
+        self.assertEqual(arrs.get([1, 2, 3], -2, "test"), "test")
 
     def test_get_empty_array(self):
         with self.assertRaises(IndexError):
@@ -14,3 +15,6 @@ class TestArrs(unittest.TestCase):
     def test_slice(self):
         self.assertEqual(arrs.my_slice([1, 2, 3, 4], 1, 3), [2, 3])
         self.assertEqual(arrs.my_slice([1, 2, 3], 1), [2, 3])
+        self.assertEqual(arrs.my_slice([], 3), [])
+        self.assertEqual(arrs.my_slice([1, 2, 3, 4], -1), [4])
+        self.assertEqual(arrs.my_slice([1, 2, 3], -4), [1, 2, 3])
